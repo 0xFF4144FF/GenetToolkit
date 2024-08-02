51ca16e0fd2e9a91b7f6ca313ca71da0d612feb0
Abderrahmane <110853324+0xFF4144FF@users.noreply.github.com>
2024-08-02 22:06:07 +0100

    Add Decoding

--- lib/pages/encoding/decoding.dart
+++ lib/pages/encoding/decoding.dart
@@ -1,15 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:genet/utils/encoding.dart';
 
 class Decode extends StatefulWidget {
   const Decode({super.key});
 
   @override
-  State<Decode> createState() => _DecodingState();
+  State<Decode> createState() => _EncodingState();
 }
 
-class _DecodingState extends State<Decode> {
+class _EncodingState extends State<Decode> {
+ 
+  String dropdownValue = encodingdecodingList.first;
+  final inputController = TextEditingController(); 
+  final outputController = TextEditingController(); 
+
+  @override
+  void dispose() {
+    outputController.dispose();
+    inputController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(20, 23, 59, 1),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                Column( 
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 30),
+                    const Center(child: Text(':: Decoding ::', style: TextStyle(color: Colors.white, fontSize: 20),)),
+                    const SizedBox(height: 50),
+                    DropdownButton<String>(
+                      value: dropdownValue,
+                      onChanged: (String? value){
+                        if (value is String){ 
+                          setState(() {
+                            dropdownValue = value;
+                          });
+                        }
+                      },
+                      items: encodingdecodingList.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(                      
+                          value: value,
+                          child: Text(value ,style: const TextStyle(color: Colors.white, fontSize: 15)),
+                        );
+                      }).toList(),
+                      iconEnabledColor: Colors.white,
+                      dropdownColor: Colors.orange,
+                      isExpanded: true,         
+                      icon: const Icon(Icons.enhanced_encryption),     
+                    ),
+                    const SizedBox(height: 50),
+                    TextField(
+                      controller: inputController,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        labelStyle: TextStyle(color: Colors.orange),
+                        hintStyle: TextStyle(color: Colors.white, fontSize: 13),
+                        hintText: 'Input goes here...',
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    TextField(
+                      controller: outputController,
+                      readOnly: true,
+                      maxLines: 5,
+                      style: const TextStyle(color: Colors.white),                   
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),                      
+                        ),
+                        hintStyle: TextStyle(color: Colors.white, fontSize: 13),
+                        hintText: 'Result goes here...',             
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      height: 50,
+                      child: FilledButton(
+                        onPressed: () {
+                          setState(() {
+                            outputController.text = decodeString(dropdownValue, inputController.text);
+                          });
+                        }, 
+                        child: const Text('Decode'),
+                      ),
+                    ),
+                  ],
+                ),
+              ]
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/utils/encoding.dart
+++ lib/utils/encoding.dart
@@ -8,6 +8,8 @@ const String iLATIN1 = 'Latin-1';
 
 List<String> encodingdecodingList = [iBASE64, iUTF8, iASCII, iLATIN1];
 
+/////////////////// ENCODING /////////////////// 
+
 String base64Encode(String input) {
   final base64Encoder = base64.encoder;
   return base64Encoder.convert(input.codeUnits);
@@ -41,6 +43,48 @@ String encodeString(String format, String input) {
       case iUTF8   : outputResult = utf8Encode(input); 
       case iASCII  : outputResult = asciiEncode(input); 
       case iLATIN1 : outputResult = latin1Encode(input);
+    }
+
+  } catch (e) {
+    outputResult = 'Incorrect Format!';
+  }
+  return outputResult;
+}
+
+/////////////////// DECODING /////////////////// 
+
+String base64Decode(String input) {
+  return utf8.decode(base64.decode(input));
+}
+
+String utf8Decode(String input) {
+  final utf8Decoder = utf8.decoder;
+  return utf8Decoder.convert(hex.decode(input));
+}
+
+String asciiDecode(String input) {
+  return ascii.decode(hex.decode(input));
+}
+
+String latin1Decode(String input) {
+  return latin1.decode(hex.decode(input));
+}
+
+String decodeString(String format, String input) {
+  String outputResult = '';
+  try {  
+    if (format.isEmpty) {
+      return 'Please Select Format!';
+    } 
+    if (input.isEmpty) {
+      return 'Please Fill The Input!';
+    }
+
+    switch (format) {
+      case iBASE64 : outputResult = base64Decode(input); 
+      case iUTF8   : outputResult = utf8Decode(input); 
+      case iASCII  : outputResult = asciiDecode(input); 
+      case iLATIN1 : outputResult = latin1Decode(input);
     }
 
   } catch (e) {
